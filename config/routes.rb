240277b80a5429e6013bb033d56a46a5a9557b4c
Bernadette Davis <bsd231@nyu.edu>
2019-11-28 19:02:43 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update] #run "$rake routes" to see key/value (pathname/path) pairs and folder paths
+  patch 'posts/:id', to: 'posts#update'
 end
