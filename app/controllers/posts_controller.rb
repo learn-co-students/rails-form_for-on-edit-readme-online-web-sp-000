3952cceed370a6b6d84127c197de32e688aa9bdc
Alexander M. Clamor <alexander.clamor@gmail.com>
2020-10-28 20:34:19 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,10 @@ class PostsController < ApplicationController
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(title: params[:title], description: params[:description])
+	  @post.update(params.require(:post).permit(:title, :description)) 
+	  #@post.update(title: params[:title], description: params[:description]) ## this won't work because, technically, you would
+	  # need to write as '@post.update(title: params[:post][:title], description: params[:post][:description])' since 'forms_for' adds in the model to the root of the params hash. So, in order to avoid typing out the model name in params, and thus be consistent
+	  # with how params are referred to in the create method, we need to require post and permit attributes title, and description.
 	  redirect_to post_path(@post)
 	end
 end
